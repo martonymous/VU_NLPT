3694c2deaa8534760c0cd2fc624cbf5e49eb6c53
martonovic <70942971+martonovic@users.noreply.github.com>
2022-05-10 12:14:23 +0200

    Add files via upload

---
+++ assignment2/main.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import random
+from sklearn.metrics import classification_report
+from simpletransformers.classification import ClassificationModel, ClassificationArgs
+# from pytorch_pretrained_bert import BertTokenizer, BertConfig
+# from pytorch_pretrained_bert import BertAdam, BertForSequenceClassification
+# import torch
+
+random_seed = 1
+random.seed(random_seed)
+
+def load_data():
+    olid_train = pd.read_csv('data/olid-train.csv')
+    olid_test = pd.read_csv('data/olid-test.csv')
+    olid_diagnostic = pd.read_csv('data/olid-subset-diagnostic-tests.csv')
+    return olid_train, olid_test, olid_diagnostic
+
+
+def random_baseline(train: pd.DataFrame, test: pd.DataFrame):
+    train_labels = train['labels'].unique()
+    pred = [random.choice(train_labels) for i in range(test.shape[0])]
+    print('\nClassification Report - Random Baseline\n', classification_report(test['labels'], pred, target_names=['Not Offensive', 'Offensive']))
+
+def majority_baseline(train: pd.DataFrame, test: pd.DataFrame):
+    maj_label = train['labels'].value_counts().argmax()
+    pred = [maj_label] * test.shape[0]
+    print('\nClassification Report - Majority Baseline\n', classification_report(test['labels'], pred, target_names=['Not Offensive', 'Offensive']))
+
+def load_bert():
+    model_args = ClassificationArgs(num_train_epochs=20, train_batch_size=8, learning_rate=0.000001, overwrite_output_dir=True, dataloader_num_workers=20)
+    bert = ClassificationModel('bert', 'bert-base-cased', args=model_args)
+    return bert
+
+
+if __name__ == '__main__':
+
+    """ Assignment 2A """
+
+    # load data
+    train, test, diagnostic = load_data()
+
+    # get class distributions and examples
+    print('\nClass distribution:\n', train['labels'].value_counts())
+    print('\nRelative distribution:\n', train['labels'].value_counts(normalize=True))
+    sns.countplot(data=train, x='labels')
+
+    train_sample_off = train[train['labels'] == 1].sample(n=1, random_state=random_seed)
+    train_sample_not = train[train['labels'] == 0].sample(n=1, random_state=random_seed)
+
+    print('EXAMPLE - Offensive:     ', train_sample_off['text'].values)
+    print('EXAMPLE - Not Offensive: ', train_sample_not['text'].values)
+
+    # plt.show()
+
+    # run baseline predictions
+    random_baseline(train, test)
+    majority_baseline(train, test)
+
+    # load and retrain bert
+    berty = load_bert()
+    berty.train_model(train)
+
+    # Evaluate the model
+    result, model_outputs, wrong_predictions = berty.eval_model(test)
+    print(result)
+    print(model_outputs)
+    print(wrong_predictions)
+    plt.show()
+
+
+
